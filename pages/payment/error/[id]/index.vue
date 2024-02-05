e8f7561f9a2579afbe294e6d443f5cd38eaf9d99
workamirhossein <workamirhossein@gmail.com>
2024-02-05 15:26:19 +0330

    payment dition:

---
+++ pages/payment/error/[id]/index.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="w-full">
+    <!------ Header Hero ----->
+    <div class="w-full mt-12 md:mt-24 flex  items-center justify-center overflow-hidden relative">
+      <div class="w-full h-[450px] flex justify-center items-center rounded-b-[30px]" style=" background: url('/images/test/success_hero.jpg') center center no-repeat; background-size: cover">
+        <div class="w-[270px] h-[270px] bg-white/[.9] rounded-full flex justify-center items-center">
+          <div class="text-center">
+            <div class="flex justify-center">
+              <img src="/images/icons/error_handling.svg" alt="payment_error">
+            </div>
+            <p class="w-full text-center font-morabaa text-[28px] mb-3 text-[#455A64]" dir="rtl">پرداخت ناموفق!</p>
+          </div>
+        </div>
+      </div>
+
+      <!------- success text and white field ------>
+    </div>
+
+    <!----- white space ----->
+    <div class="w-full">
+      <p class="w-full text-zinc-400 mt-4 text-[34px] text-center font-morabaa" dir="rtl">خطایی در پرداخت پیش آمده!</p>
+
+      <!----- datas table ---->
+      <div class="w-full mt-5">
+        <div class="flex justify-between p-4 border-b-2 mx-auto border-zinc-200 w-11/12 md:w-6/12" dir="rtl">
+          <p>تاریخ / زمان :</p>
+          <p>1402/8/10 / 15:24:35</p>
+        </div>
+        <div class="flex justify-between p-4 border-b-2 mx-auto border-zinc-200 w-11/12 md:w-6/12" dir="rtl">
+          <p>تاریخ / زمان :</p>
+          <p>1402/8/10 / 15:24:35</p>
+        </div>
+        <div class="flex justify-between p-4 border-b-2 mx-auto border-zinc-200 w-11/12 md:w-6/12" dir="rtl">
+          <p>تاریخ / زمان :</p>
+          <p>1402/8/10 / 15:24:35</p>
+        </div>
+        <div class="flex justify-between p-4 border-b-2 mx-auto border-zinc-200 w-11/12 md:w-6/12" dir="rtl">
+          <p>تاریخ / زمان :</p>
+          <p>1402/8/10 / 15:24:35</p>
+        </div>
+        <div class="flex justify-between p-4 border-b-2 mx-auto border-zinc-200 w-11/12 md:w-6/12" dir="rtl">
+          <p>تاریخ / زمان :</p>
+          <p>1402/8/10 / 15:24:35</p>
+        </div>
+
+      </div>
+
+      <div class="mt-8 w-11/12 md:w-[500px] mx-auto">
+        <div class="w-full border border-2 transition-all hover:border-zinc-500 mt-4 border-[#0093A9] flex items-center justify-center p-2">
+          <p class=" text-[#0093A9]">بازگشت به خانه</p>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#0093A9" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+          </svg>
+        </div>
+
+      </div>
+
+
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+
+definePageMeta({
+  layout: 'default',
+})
+
+</script>
+
+<style scoped>
+
+</style>
