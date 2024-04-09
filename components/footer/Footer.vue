af2eb89b7f1cb577cc96c144b1a4bd89859c6c9d
workamirhossein <workamirhossein@gmail.com>
2024-04-09 10:41:43 +0330

    final

--- components/footer/Footer.vue
+++ components/footer/Footer.vue
@@ -2,11 +2,12 @@
 
   <div class="w-full overflow-hidden relative mt-24">
     <!-----desktop footer path-------->
-<!--    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1512 622" fill="none" class="w-full hidden lg:block lg:mt-10">-->
-<!--      <path d="M1512 0L0 125.5V622H1512V0Z" fill="#002D4B"/>-->
-<!--    </svg>-->
+    <!--    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1512 622" fill="none" class="w-full hidden lg:block lg:mt-10">-->
+    <!--      <path d="M1512 0L0 125.5V622H1512V0Z" fill="#002D4B"/>-->
+    <!--    </svg>-->
 
-    <div class="w-full p-4 min-h-[500px] bg-[#002D4B] hidden lg:block" style="clip-path: polygon(0 18%, 100% 0, 100% 100%, 0% 100%);">
+    <div class="w-full p-4 min-h-[500px] bg-[#002D4B] hidden lg:block"
+         style="clip-path: polygon(0 18%, 100% 0, 100% 100%, 0% 100%);">
 
     </div>
 
@@ -31,18 +32,23 @@
             سرمایه‌ی کم به دنیای سرمایه‌گذاری ملکی وارد شوید.
           </p>
 
-          <div class="text-sm flex items-center w-10/12 mx-auto text-right text-white justify-center mt-3 flex gap-2">
-            <a referrerpolicy="origin" target="_blank"
-               href="https://trustseal.enamad.ir/?id=364419&amp;Code=famE1evKQ2jNA9Bhl8Hd"><img referrerpolicy="origin"
-                                                                                                src="https://Trustseal.eNamad.ir/logo.aspx?id=364419&amp;Code=famE1evKQ2jNA9Bhl8Hd"
-                                                                                                alt=""
-                                                                                                style="cursor:pointer"
-                                                                                                id="famE1evKQ2jNA9Bhl8Hd">
+          <div class="text-sm flex items-center w-10/12 mx-auto text-right text-white justify-center mt-3 flex gap-2 flex-wrap">
+            <a referrerpolicy="origin" target="_blank" style="width: 64px !important;"
+               href="https://trustseal.enamad.ir/?id=364419&amp;Code=famE1evKQ2jNA9Bhl8Hd">
+              <img referrerpolicy="origin"
+                   src="https://Trustseal.eNamad.ir/logo.aspx?id=364419&amp;Code=famE1evKQ2jNA9Bhl8Hd"
+                   alt=""
+                   style="cursor:pointer; width: 64px !important;"
+                   id="famE1evKQ2jNA9Bhl8Hd">
             </a>
-            <nuxt-link to="https://tehran.irannsr.org/fa/trade_unit/2504716/swd_id/104366/%D8%A7%D8%B9%D8%B6%D8%A7%DB%8C-%D8%AD%D9%82%D9%88%D9%82%DB%8C-%D8%A7%DB%8C%D8%AF%D9%87-%D9%BE%D8%B1%D8%AF%D8%A7%D8%B2%D8%A7%D9%86-%D8%B3%D8%A7%D8%B2%D9%87-%DA%AF%D8%B3%D8%AA%D8%B1-%D8%B3%D9%88%D9%85%D8%A7.html">
-              <img src="/images/senfi.png" alt="rayane_senf_logo" class="w-[64px] h-[56px]" style="width: 64px !important;">
+            <nuxt-link
+                to="https://tehran.irannsr.org/fa/trade_unit/2504716/swd_id/104366/%D8%A7%D8%B9%D8%B6%D8%A7%DB%8C-%D8%AD%D9%82%D9%88%D9%82%DB%8C-%D8%A7%DB%8C%D8%AF%D9%87-%D9%BE%D8%B1%D8%AF%D8%A7%D8%B2%D8%A7%D9%86-%D8%B3%D8%A7%D8%B2%D9%87-%DA%AF%D8%B3%D8%AA%D8%B1-%D8%B3%D9%88%D9%85%D8%A7.html">
+              <img src="/images/senfi.png" alt="rayane_senf_logo" class="w-[64px] h-[56px]"
+                   style="width: 64px !important; height: 56px !important;">
             </nuxt-link>
-            <img referrerpolicy='origin' id = 'rgvjfukzapfufukzfukzesgt' style = 'cursor:pointer; width: 64px !important; ' onclick = 'window.open("https://logo.samandehi.ir/Verify.aspx?id=365660&p=xlaogvkadshwgvkagvkaobpd", "Popup","toolbar=no, scrollbars=no, location=no, statusbar=no, menubar=no, resizable=0, width=64, height=64, top=30")' alt = 'logo-samandehi' src = 'https://logo.samandehi.ir/logo.aspx?id=365660&p=qftiwlbqujynwlbqwlbqlyma' />
+            <img referrerpolicy='origin' id='rgvjfukzapfufukzfukzesgt' style='cursor:pointer; width: 64px !important; '
+                 onclick='window.open("https://logo.samandehi.ir/Verify.aspx?id=365660&p=xlaogvkadshwgvkagvkaobpd", "Popup","toolbar=no, scrollbars=no, location=no, statusbar=no, menubar=no, resizable=0, width=64, height=64, top=30")'
+                 alt='logo-samandehi' src='https://logo.samandehi.ir/logo.aspx?id=365660&p=qftiwlbqujynwlbqwlbqlyma'/>
 
             <NuxtLink to="https://loogano.com/endpoints/ups/loogano_cert.pdf" target="_blank" :external="true">
               <img src="public/images/ershad.svg" class="cursor-pointer" alt="ershad" style="width: 64px !important;">
@@ -241,20 +247,27 @@
           <hr class="my-2">
 
           <div class="flex items-center justify-center flex-wrap">
-            <a referrerpolicy="origin" target="_blank"
-               href="https://trustseal.enamad.ir/?id=364419&amp;Code=famE1evKQ2jNA9Bhl8Hd"><img referrerpolicy="origin"
-                                                                                                src="https://Trustseal.eNamad.ir/logo.aspx?id=364419&amp;Code=famE1evKQ2jNA9Bhl8Hd"
-                                                                                                alt=""
-                                                                                                style="cursor:pointer"
-                                                                                                id="famE1evKQ2jNA9Bhl8Hd">
+            <a referrerpolicy="origin" style="width: 64px !important; height: 70px !important;" target="_blank"
+               href="https://trustseal.enamad.ir/?id=364419&amp;Code=famE1evKQ2jNA9Bhl8Hd">
+              <img referrerpolicy="origin"
+                   src="https://Trustseal.eNamad.ir/logo.aspx?id=364419&amp;Code=famE1evKQ2jNA9Bhl8Hd"
+                   alt=""
+                   style="cursor:pointer; width: 64px !important;"
+                   id="famE1evKQ2jNA9Bhl8Hd">
             </a>
-            <nuxt-link to="https://tehran.irannsr.org/fa/trade_unit/2504716/swd_id/104366/%D8%A7%D8%B9%D8%B6%D8%A7%DB%8C-%D8%AD%D9%82%D9%88%D9%82%DB%8C-%D8%A7%DB%8C%D8%AF%D9%87-%D9%BE%D8%B1%D8%AF%D8%A7%D8%B2%D8%A7%D9%86-%D8%B3%D8%A7%D8%B2%D9%87-%DA%AF%D8%B3%D8%AA%D8%B1-%D8%B3%D9%88%D9%85%D8%A7.html">
-              <img src="/images/senfi.png" alt="rayane_senf_logo" class="w-[64px] h-[56px]" style="width: 64px !important;">
+            <nuxt-link
+                to="https://tehran.irannsr.org/fa/trade_unit/2504716/swd_id/104366/%D8%A7%D8%B9%D8%B6%D8%A7%DB%8C-%D8%AD%D9%82%D9%88%D9%82%DB%8C-%D8%A7%DB%8C%D8%AF%D9%87-%D9%BE%D8%B1%D8%AF%D8%A7%D8%B2%D8%A7%D9%86-%D8%B3%D8%A7%D8%B2%D9%87-%DA%AF%D8%B3%D8%AA%D8%B1-%D8%B3%D9%88%D9%85%D8%A7.html">
+              <img src="/images/senfi.png" alt="rayane_senf_logo"
+                   style="width: 64px !important; height: 56px !important;">
             </nuxt-link>
-            <img referrerpolicy='origin' id = 'rgvjfukzapfufukzfukzesgt' style = 'cursor:pointer; width: 64px !important; ' onclick = 'window.open("https://logo.samandehi.ir/Verify.aspx?id=365660&p=xlaogvkadshwgvkagvkaobpd", "Popup","toolbar=no, scrollbars=no, location=no, statusbar=no, menubar=no, resizable=0, width=64, height=64, top=30")' alt = 'logo-samandehi' src = 'https://logo.samandehi.ir/logo.aspx?id=365660&p=qftiwlbqujynwlbqwlbqlyma' />
+            <img referrerpolicy='origin' id='rgvjfukzapfufukzfukzesgt'
+                 style='cursor:pointer; width: 64px !important; height: 64px !important; '
+                 onclick='window.open("https://logo.samandehi.ir/Verify.aspx?id=365660&p=xlaogvkadshwgvkagvkaobpd", "Popup","toolbar=no, scrollbars=no, location=no, statusbar=no, menubar=no, resizable=0, width=64, height=64, top=30")'
+                 alt='logo-samandehi' src='https://logo.samandehi.ir/logo.aspx?id=365660&p=qftiwlbqujynwlbqwlbqlyma'/>
 
             <NuxtLink to="https://loogano.com/endpoints/ups/loogano_cert.pdf" target="_blank" :external="true">
-              <img src="public/images/ershad.svg" class="cursor-pointer" alt="ershad" style="width: 64px !important;">
+              <img src="public/images/ershad.svg" class="cursor-pointer" alt="ershad"
+                   style="width: 64px !important; height: 71px !important;">
             </NuxtLink>
           </div>
 
@@ -270,9 +283,6 @@
 </template>
 
 <script setup>
-
-
-
 
 
 const showErshad = () => {
